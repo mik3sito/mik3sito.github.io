0b172b2edb631590f633938148aee44ee99554ca
Miguel Condés <mike17798@gmail.com>
2019-12-02 10:16:38 +0100

    Ejercicio 42 JS

---
+++ Cliente/1Eval/Practica42/js/codigo.js
@@ -0,0 +1,12 @@
+function loadDoc() {
+  var nombre=document.getElementById("nombre").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", nombre+".txt", true);
+  xhttp.send();
+}
